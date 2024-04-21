5ac8d0d6a65575befdc6598126729be3d532b4c7
ma <501847822@qq.com>
2024-04-21 10:33:21 +0800

    add 增加网关

---
+++ maapi-gateway/src/main/java/com/mahua/maapigateway/MaapiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mahua.maapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaapiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaapiGatewayApplication.class, args);
+	}
+
+}
