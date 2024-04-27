2ab14a0f19c40d23a04e8ab811e5ab208fd740eb
ma <501847822@qq.com>
2024-04-27 16:02:04 +0800

    update 接口权限管理

---
+++ maapi-common/src/test/java/com/mahua/maapicommon/MaapiCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahua.maapicommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaapiCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
